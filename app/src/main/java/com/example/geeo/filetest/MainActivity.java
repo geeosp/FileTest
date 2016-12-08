7bbff8cf44257f6c8250dab2ff4077f76a553256
Geovane Pereira <geeo.sp@gmail.com>
2016-12-08 11:57:34 -0300

    oi

--- app/src/main/java/com/example/geeo/filetest/MainActivity.java
+++ app/src/main/java/com/example/geeo/filetest/MainActivity.java
@@ -1,13 +1,142 @@
 package com.example.geeo.filetest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import droidninja.filepicker.FilePickerBuilder;
+import droidninja.filepicker.FilePickerConst;
+
 
 public class MainActivity extends AppCompatActivity {
+    final int FILE_CHOOSER = 1;
+
+    TextView tv_debug;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        permissionsRequest();
+        tv_debug = (TextView) findViewById(R.id.tv_debug);
     }
+
+
+    public void pickFile(View v) {
+        FilePickerBuilder.getInstance().setMaxCount(1)
+                //     .setSelectedFiles(filePaths)
+                .setActivityTheme(R.style.AppTheme)
+
+                .pickDocument(this);
+    }
+
+    public void pickMedia(View v){
+
+        FilePickerBuilder.getInstance().setMaxCount(1)
+                //     .setSelectedFiles(filePaths)
+                .setActivityTheme(R.style.AppTheme)
+
+                .pickPhoto(this);
+    }
+
+
+    public void openPickedFile(View v) {
+
+
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String debug ="";
+        List<String> photoPaths;
+        List<String> docPaths;
+
+        switch (requestCode) {
+            case FilePickerConst.REQUEST_CODE_PHOTO:
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    photoPaths = new ArrayList<>();
+                    photoPaths.addAll(data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_PHOTOS));
+                    debug += photoPaths.get(0);
+
+
+
+                }
+                break;
+            case FilePickerConst.REQUEST_CODE_DOC:
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    docPaths = new ArrayList<>();
+                    docPaths.addAll(data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_DOCS));
+                    debug += docPaths.get(0);
+                }
+                break;
+        }
+
+
+        tv_debug.setText(debug);
+
+    }
+
+
+    // Requests app permissions
+    public void permissionsRequest() {
+        Context ctx = getApplicationContext();
+        if (ContextCompat.checkSelfPermission(ctx,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(ctx,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(ctx,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(ctx,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 111);
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 111: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    permissionsNotSelected();
+                }
+            }
+        }
+    }
+
+
+    private void permissionsNotSelected() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissions Requred");
+        builder.setMessage("Please enable the requested permissions in the app settings in order to use this demo app");
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+                System.exit(1);
+            }
+        });
+        AlertDialog noInternet = builder.create();
+        noInternet.show();
+    }
+
 }
